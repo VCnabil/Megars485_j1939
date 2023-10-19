9502e0c1b4aa45f5964c99e1cbf520a777025fc7
Nabil <nlamriben@vcinc.com>
2023-10-19 10:50:33 -0400

    clean Working single channel rs485 and send can clean

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,11 @@ byte rez14_b1_HIGH;
 MCP_CAN CAN0(10);     // Set CS to pin 10
 byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
 void setup() {
+  Serial.begin(115200);  
   pinMode(DE_RE_PIN, OUTPUT);
   digitalWrite(DE_RE_PIN, LOW); // Initialize DE/RE pin to LOW for receiving
   Serial1.begin(2000000);  
-  Serial.begin(115200);  
   SPI.begin(); 
-    // Initialize MCP2515 running at 8MHz with a baudrate of 500kb/s and the masks and filters disabled.
   if(CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
   else Serial.println("Error Initializing MCP2515...");
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
