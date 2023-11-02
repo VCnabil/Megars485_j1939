e0a52b030cdb8b7651a2a22f08d4a4855ee7fdaa
Nabil <nlamriben@vcinc.com>
2023-11-02 16:38:20 -0400

    sweet refactored code using no delays and a timing queue instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,12 @@
-//works as is as of 11/1/2023 Wednesday Zeroing senssor 24 with button 
+//works as is as of 11/2/2023 Thursday Zeroing senssors without delay! 
 #include <Arduino.h>
 #include <mcp_can.h>
-
 //********************************manip these
 bool debug_serialRead=false;
 bool debug_printValues=false;
 unsigned long pingInterval = 1;
 unsigned long PrintDebugInterval = 500;
 unsigned long SendCanIntervalMS = 100;
-//********************************DONT TOUCH THESE Zeroing
-const int buttonPin = 3;
-int buttonState = 0;   
-unsigned long lastRunTime_24_zero = 0;  
-unsigned long lastFinishZeroing_24_Time = 0;
-bool isZeroing_24=false;
-const unsigned long interval_zero = 200; 
 //********************************DONT TOUCH THESE Read CAN
 long unsigned int rxId;
 unsigned char len = 0;
@@ -23,30 +15,91 @@ char msgString[128];
 #define CAN0_INT 2
 long unsigned int PGNzeroing = 0x18FF0100;
 //********************************DONT TOUCH THESE
-unsigned long lastPing14_A = 0;
-unsigned long lastPing24_B = 0;
-unsigned long lastPing34_C = 0;
-unsigned long lastPing44_D = 0;
 unsigned long lastPrintDebug = 0;
 unsigned long lastCanSend = 0;
-byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
-int rez12_14A = 0;
-int rez12_24B = 0;
-int rez12_34C = 0;
-int rez12_44D = 0;
+//********************************Zeroing without delay
+const int MAX_ZERO_QUEUE = 4; // Maximum number of sensors to zero at once
+byte zeroQueue[MAX_ZERO_QUEUE];
+int zeroQueueIndex = 0;
+unsigned long lastZeroSensorTime = 0;
+unsigned long ZeroingIntervals = 20;
 MCP_CAN CAN0(10);
- 
+
+// Sensor struct to encapsulate sensor-related data and operations
+struct Sensor {
+  byte id;
+  unsigned long lastPing;
+  int rez12;
+  HardwareSerial &serialPort;
+  Sensor(byte _id, HardwareSerial &_serialPort) : id(_id), lastPing(0), rez12(0), serialPort(_serialPort) {}
+  int processSensorData(byte argdata[2]) {
+    int rez12 = (((int)(argdata[1] & 0x3F) << 8) | argdata[0]) >> 2;
+    return rez12;
+  }
+void handlePing() {
+    if (millis() - lastPing >= pingInterval) {
+      lastPing = millis();
+      serialPort.write(id);
+      delay(1);
+      if (serialPort.available() == 2) {
+        byte Data_rec[2];
+        Data_rec[0] = serialPort.read();
+        Data_rec[1] = serialPort.read();
+        rez12 = processSensorData(Data_rec);
+      } else {
+        if (debug_serialRead) {
+          Serial.print("No data ");
+          Serial.println(id, HEX);
+        }
+      }
+      while (serialPort.available()) serialPort.read();
+    }
+  }
+};
+
+// Global sensor objects
+Sensor sensors[] = {
+  {0x14, Serial3},
+  {0x24, Serial3},
+  {0x34, Serial3},
+  {0x44, Serial3}
+};
+
+void addToZeroQueue(byte sensorID) {
+  if (zeroQueueIndex < MAX_ZERO_QUEUE) {
+    zeroQueue[zeroQueueIndex++] = sensorID;
+  }
+}
+
+void zeroSensor() {
+  if (zeroQueueIndex > 0) {
+    if (millis() - lastZeroSensorTime >= ZeroingIntervals) {
+      byte sensorID = zeroQueue[0];
+      if (debug_printValues) {
+        Serial.print("zeroing ");
+        Serial.println(sensorID, HEX);
+      }
+      Serial3.write(sensorID + 2);  // extended ID
+      Serial3.write(0x5E);  // zero command
+      lastZeroSensorTime = millis();
+      // Shift the queue
+      for (int i = 1; i < zeroQueueIndex; i++) {
+        zeroQueue[i - 1] = zeroQueue[i];
+      }
+      zeroQueueIndex--;
+    }
+  }
+}
+
 void initSerial() {
-   pinMode(buttonPin, INPUT);//********************Zeroing
   Serial.begin(115200);
   delay(200);
   Serial3.begin(115200);
   Serial.println("RS485-Test");
-  delay(200);  
+  delay(200);
 }
 
 void initCAN() {
-   
   if (CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK) {
     Serial.println("MCP2515 Initialized Successfully!");
   } else {
@@ -55,145 +108,59 @@ void initCAN() {
   CAN0.setMode(MCP_NORMAL);
 }
 
-int processSensorData(byte data[2]) {
-  int rez12 = (((int)(data[1] & 0x3F) << 8) | data[0]) >> 2;
-  return rez12;
-}
-
-void handleGeneralPing(byte sensorID, int &argrez12, HardwareSerial &serialPort) {
-  unsigned long &lastPing = (sensorID == 0x14) ? lastPing14_A : 
-                            (sensorID == 0x24) ? lastPing24_B : 
-                            (sensorID == 0x34) ? lastPing34_C : 
-                            (sensorID == 0x44) ? lastPing44_D : 
-                            lastPing24_B;
-
-  if (millis() - lastPing >= pingInterval) {
-    lastPing = millis();
-    serialPort.write(sensorID);
-    delay(1);
-    if (serialPort.available() == 2) {
-      byte Data_rec[2];
-      Data_rec[0] = serialPort.read();
-      Data_rec[1] = serialPort.read();
-      argrez12 = processSensorData(Data_rec);
-    } else {
-      if(debug_serialRead){
-        Serial.print("No data ");
-        Serial.println(sensorID, HEX);
-      }
-    }
-    while (serialPort.available()) serialPort.read();
-  }
-}
-
-void printValues() {
-  if(!debug_printValues)return;
-  if (millis() - lastPrintDebug >= PrintDebugInterval) {
-    lastPrintDebug = millis();
-    Serial.print(" 14A: "); Serial.print(rez12_14A); Serial.print(" ");
-    Serial.print(" 24B: "); Serial.print(rez12_24B); Serial.print(" ");
-    Serial.print(" 34C: "); Serial.print(rez12_34C); Serial.print(" ");
-    Serial.print(" 44D: "); Serial.print(rez12_44D);
-    Serial.println(" ");
-  }
-}
-
 void sendCAN() {
   if (millis() - lastCanSend >= SendCanIntervalMS) {
     lastCanSend = millis();
-
-    data[0] = rez12_14A & 0xFF;
-    data[1] = (rez12_14A >> 8) & 0xFF;
-
-    data[2] = rez12_24B & 0xFF;
-    data[3] = (rez12_24B >> 8) & 0xFF;
-
-    data[4] = rez12_34C & 0xFF;
-    data[5] = (rez12_34C >> 8) & 0xFF;
-
-    data[6] = rez12_44D & 0xFF;
-    data[7] = (rez12_44D >> 8) & 0xFF;
-
+    byte data[8];
+    for (int i = 0; i < 4; i++) {
+      data[i * 2] = sensors[i].rez12 & 0xFF;
+      data[i * 2 + 1] = (sensors[i].rez12 >> 8) & 0xFF;
+    }
     CAN0.sendMsgBuf(0x18FFFA00, 1, 8, data);
   }
 }
 
-void funcZero_24_BTN() {
-  isZeroing_24=true;
-  Serial3.write(0x26); //extended for 0x24
-  Serial3.write(0x5E); // zero it 
-  lastFinishZeroing_24_Time = millis();
-}
-
-void loop_Zeroing_BTN() {
-  buttonState = digitalRead(buttonPin);
-  unsigned long currentTime = millis();
-  if (buttonState == HIGH && currentTime - lastRunTime_24_zero >= interval_zero) {
-    funcZero_24_BTN();
-    lastRunTime_24_zero = currentTime;  
-  }
-  if (isZeroing_24 && currentTime - lastFinishZeroing_24_Time >= interval_zero) {
-    Serial.println("finished");
-    isZeroing_24 = false;  // reset the flag
+void loopReadCan() {
+  if (!digitalRead(CAN0_INT)) {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);
+    if ((rxId & 0x80000000) == 0x80000000) {
+      long unsigned int recid = rxId & 0x1FFFFFFF;
+      if (recid == PGNzeroing && len >= 4) {
+        for (int i = 0; i < 4; i++) {
+          if (rxBuf[i] > 0) {
+            addToZeroQueue(sensors[i].id);
+          }
+        }
+      }
+    }
   }
 }
 
-void Zero_14A(){
- if(debug_printValues)Serial.println("zeroing A");
- Serial3.write(0x16); //extended for 0x14
- Serial3.write(0x5E); // zero it 
- delay(200);
+void printValues() {
+  if (!debug_printValues) return;
+  if (millis() - lastPrintDebug >= PrintDebugInterval) {
+    lastPrintDebug = millis();
+    for (Sensor &sensor : sensors) {
+      Serial.print(" ");
+      Serial.print(sensor.id, HEX);
+      Serial.print(": ");
+      Serial.print(sensor.rez12);
+    }
+    Serial.println();
+  }
 }
-void Zero_24B(){
- if(debug_printValues)Serial.println("zeroing B");
- Serial3.write(0x26); //extended for 0x24
- Serial3.write(0x5E); // zero it 
- delay(200);
-}
-void Zero_34C(){
- if(debug_printValues)Serial.println("zeroing C");
- Serial3.write(0x36); //extended for 0x34
- Serial3.write(0x5E); // zero it 
- delay(200);
-}
-void Zero_44D(){
- if(debug_printValues)Serial.println("zeroing D");
- Serial3.write(0x46); //extended for 0x44
- Serial3.write(0x5E); // zero it 
- delay(200);
-}
-//*********************************setup and void loop()
+//**********************************************************Setup and loop
 void setup() {
   initSerial();
   initCAN();
 }
-void loopReadCan(){
-  if(!digitalRead(CAN0_INT))  // If CAN0_INT pin is low, read receive buffer
-  {
-    CAN0.readMsgBuf(&rxId, &len, rxBuf); 
-    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is 29bit
-    {
-     long unsigned int recid =rxId & 0x1FFFFFFF;
-    if(recid == PGNzeroing){
-       if(len>=4){
-        if(rxBuf[0]>0){Zero_14A();}
-        if(rxBuf[1]>0){Zero_24B();}
-        if(rxBuf[2]>0){Zero_34C();}
-        if(rxBuf[3]>0){Zero_44D();}
-       }
-    }
-    }
-  } 
-}
+
 void loop() {
-  loop_Zeroing_BTN();
-  if(!isZeroing_24){
-    loopReadCan();
-    handleGeneralPing(0x14, rez12_14A,Serial3);
-    handleGeneralPing(0x24, rez12_24B,Serial3);
-    handleGeneralPing(0x34, rez12_34C,Serial3);
-    handleGeneralPing(0x44, rez12_44D,Serial3);
-    printValues();
-    sendCAN();
+  for (Sensor &sensor : sensors) {
+    sensor.handlePing();
   }
+  printValues();
+  sendCAN();
+  loopReadCan();
+  zeroSensor();
 }
